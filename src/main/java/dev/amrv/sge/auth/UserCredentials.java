d4cce3a1807454051c55103e525f52d2e295e1a9
Ansuz <adrianmrv02@gmail.com>
2023-11-07 11:13:19 +0100

    UserCredentials backup

--- src/main/java/dev/amrv/sge/auth/UserCredentials.java
+++ src/main/java/dev/amrv/sge/auth/UserCredentials.java
@@ -1,5 +1,6 @@
 package dev.amrv.sge.auth;
 
+import dev.amrv.sge.SGE;
 import dev.amrv.sge.SGEFileSystem;
 import dev.amrv.sge.io.PermissionsFile;
 import java.io.IOException;
@@ -12,16 +13,35 @@ import java.util.Map.Entry;
  */
 public final class UserCredentials {
 
-    private static final UserCredentials anonymous = new UserCredentials("anonymous", "");
+    private static final UserCredentials anonymous = new UserCredentials("anonymous", "anonymous");
 
-    public static UserCredentials getUserCredentials(String username, String password) {
+    public synchronized static boolean changeUsername(SGE sge, String newUsername, String newPassword, String password) throws IOException {
+        if (!sge.getUser().getPasswordHash().equals(sge.getUser().callDigest(password)))
+            return false;
 
+        final UserCredentials newCredentials = new UserCredentials(newUsername, newPassword, sge.getUser().permissions);
+
+        newCredentials.file.save();
+        sge.getUser().file.getFile().delete();
+        sge.getUser().file.getFile().deleteOnExit();
+
+        sge.setUser(newCredentials);
+        return true;
+    }
+
+    public static UserCredentials createUser(String username, String password) throws IOException {
         UserCredentials credentials = new UserCredentials(username, password);
+
+        credentials.savePermissions();
 
         if (!credentials.isValid())
             return anonymous();
 
         return credentials;
+    }
+
+    public static UserCredentials getUserCredentials(String username, String password) {
+        return new UserCredentials(username, password);
     }
 
     private final PermissionRoot permissions;
@@ -30,14 +50,24 @@ public final class UserCredentials {
     protected final PermissionsFile file;
 
     private UserCredentials(String username, String password) {
-        this.username = username;
-        this.password = callDigest(password);
-        this.permissions = new PermissionRoot();
-        this.file = new PermissionsFile(SGEFileSystem.getSourceFile("user", this.password));
+        this(username, password, null);
     }
 
-    protected boolean isValid() {
-        return true;
+    private UserCredentials(String username, String password, PermissionRoot permissions) {
+        this.username = username;
+        System.out.println("USER: " + username + " -- " + password);
+        this.password = callDigest(password);
+        this.permissions = permissions == null ? new PermissionRoot() : permissions;
+        this.file = new PermissionsFile(SGEFileSystem.getSourceFile("user", getFilename()));
+        System.out.println("FILE: " + this.file.getFile().getName());
+    }
+
+    protected String getFilename() {
+        return callDigest(username);
+    }
+
+    public boolean isValid() {
+        return file.getFile().isFile();
     }
 
     private String callDigest(String password) {
@@ -51,11 +81,18 @@ public final class UserCredentials {
     public void loadPermissions() throws IOException {
         file.read();
 
-        for (String line : file.content())
-            if (line.startsWith("!"))
-                permissions.setPermission(line.substring(1), false);
-            else
-                permissions.setPermission(line, true);
+        for (String line : file.content()) {
+            if (line.startsWith("@")) {
+                // Header
+            } else {
+                if (line.startsWith("!"))
+                    // Revoke permission
+                    permissions.setPermission(line.substring(1), false);
+                else
+                    // Grant permission
+                    permissions.setPermission(line, true);
+            }
+        }
     }
 
     public void savePermissions() throws IOException {
@@ -73,7 +110,7 @@ public final class UserCredentials {
         return username;
     }
 
-    public String getPassword() {
+    public String getPasswordHash() {
         return password;
     }
 
@@ -83,6 +120,12 @@ public final class UserCredentials {
 
     public boolean hasPermission(String permission) {
         return permissions.hasPermission(permission);
+    }
+
+    public static UserCredentials loadAnonymous() throws IOException {
+        anonymous.file.read();
+
+        return anonymous;
     }
 
     public static UserCredentials anonymous() {
